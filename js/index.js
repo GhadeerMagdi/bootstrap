623074782b87a3eb74e24117d650663704215524
Ghadeer Magdi <63327311+GhadeerMagdi@users.noreply.github.com>
2024-12-15 17:53:07 +0200

    modify the navbar

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
-let Open = $(".nav-link").outerWidth();
-let maxLength = 100;
 
-$("a").click(function(){
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $(".navbar").removeClass("bg-transparent navbar-dark").addClass("bg-white navbar-light text-dark");
+        $(".navbar .nav-link").css("color", "#343a40"); 
+        $(".navbar .active > .nav-link").css("color", "#343a40");
+    } else {
+        $(".navbar").removeClass("bg-white navbar-light text-dark").addClass("bg-transparent navbar-dark");
+        $(".navbar .nav-link").css("color", ""); 
+        $(".navbar .active > .nav-link").css("color", ""); 
+    }
+});
 
+$("a").click(function (e) {
+    e.preventDefault(); 
     let aHref = $(this).attr('href');
     let sectionOffset = $(aHref).offset().top;
-    $("html,body").animate({scrollTop:sectionOffset} , 2000)
-})
+    $("html,body").animate({ scrollTop: sectionOffset }, 2000);
+});
